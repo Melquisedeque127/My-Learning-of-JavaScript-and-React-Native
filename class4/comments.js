e3d9598086894d3988734f0b02dcbed1523494f6
Melquisedeque154 <>
2024-07-11 10:15:27 -0300

    class4\comments.js

---
+++ class4/comments.js
@@ -0,0 +1,9 @@
+// E escreve um comentário longo.
+console.log('Olá , Mundo!') // Aqui tem outro comeentário.
+console.log("Linha 1")
+/*
+//console.log("Linha 2")
+console.log("Linha 3")
+console.log("Linha 4")
+*/
+// Vai ser ignorado.
