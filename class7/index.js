9a858d1f0f69681338a60369d6ccc48ac760b97c
Melquisedeque154 <>
2024-07-19 10:56:06 -0300

    class7\index.js

---
+++ class7/index.js
@@ -0,0 +1,25 @@
+/*
+* Não podemos cria constantes com palavras reservadas.
+* Constantes precisam ter nomes significativos.
+* Não pode começar com o nome de uma constante com um número.
+* Não podem conter espaços ou traços.
+* Utilizamos camelCase
+* Case-sensitive
+* Não pode modificar o valor de uma constante.
+* NÃO UTILIZE VAR, UTILIZE CONST.
+*/
+
+const nome = 'João';
+//console.log(nome);
+
+// Strig = Text | Number = Número
+// + - * /
+const primeroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+//console.log(primeroNumero);
+
+console.log (typeof (primeroNumero + segundoNumero))
